aab5104bdcfbf70403c2405666d595b6ca02df5e
anon <anon@mail.com>
2024-06-21 17:09:31 +0800

    Add logo to dashboard

--- resources/js/components/Logo.jsx
+++ resources/js/components/Logo.jsx
@@ -1,21 +1,12 @@
-import { Center, Group, Text, rem, useComputedColorScheme } from "@mantine/core";
+import { Container, useComputedColorScheme, Image } from "@mantine/core";
 import { IconChartArcs } from "@tabler/icons-react";
 
 export default function Logo(props) {
   const computedColorScheme = useComputedColorScheme();
 
   return (
-    <Group wrap="nowrap" {...props}>
-      <Center
-        bg={computedColorScheme === "dark" ? "blue.8" : "blue.9"}
-        p={5}
-        style={{ borderRadius: "100%" }}
-      >
-        <IconChartArcs style={{ stroke: "#fff", width: rem(25), height: rem(25), flexShrink: 0 }} />
-      </Center>
-      <Text fz={20} fw={600}>
-        LaraCollab
-      </Text>
-    </Group>
+    <Container p={16}>
+      <Image h={32} src="assets/redbean-wordmark/rb-wm-svg-red.svg" />
+    </Container>
   );
 }
